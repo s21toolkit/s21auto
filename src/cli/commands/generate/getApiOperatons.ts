04bcbb6ab792414b08b487ad2109637943998b5c
bgenia <bgenia@student.21-school.ru>
2023-08-27 05:38:49 +0300

    refactor: reimplemented generate command

---
+++ src/cli/commands/generate/getApiOperatons.ts
@@ -0,0 +1,30 @@
+import { Entry, Har } from "har-format"
+import { GqlRequest } from "@/gql/GqlRequest"
+import { filter, isGqlRequest, isHttpOk } from "@/har/filter"
+import { pipe } from "@/utils/pipe"
+
+export function getApiOperations(har: Har) {
+	const filteredHar = pipe
+		.of(har)
+		.then((har) => filter(har, isGqlRequest))
+		.then((har) => filter(har, isHttpOk))
+		.call()
+
+	const operations = new Map<string, [Entry, ...Entry[]]>()
+
+	for (const entry of filteredHar.log.entries) {
+		const requestData = entry.request.postData?.text
+
+		if (!requestData) continue
+
+		const { operationName } = JSON.parse(requestData) as GqlRequest
+
+		if (operations.has(operationName)) {
+			operations.get(operationName)!.push(entry)
+		} else {
+			operations.set(operationName, [entry])
+		}
+	}
+
+	return operations
+}
--- src/cli/commands/generate/index.ts
+++ src/cli/commands/generate/index.ts
@@ -1,15 +1,12 @@
 import { writeFile } from "fs/promises"
 import { resolve } from "path"
 import { command } from "cmd-ts"
-import { Entry, Har } from "har-format"
 import { harFiles } from "@/cli/arguments/harFiles"
 import { outDir } from "@/cli/arguments/outDir"
-import { generate } from "@/cli/commands/generate/generate"
-import { GqlRequest } from "@/gql/GqlRequest"
-import { GqlResponse } from "@/gql/GqlResponse"
-import { filter, isGqlRequest, isHttpOk } from "@/har/filter"
+import { generateRequestFile } from "@/cli/commands/generate/generateRequestFile"
+import { getApiOperations } from "@/cli/commands/generate/getApiOperatons"
+import { getDataSamples } from "@/cli/commands/generate/getDataSamples"
 import { merge } from "@/har/merge"
-import { pipe } from "@/utils/pipe"
 
 export const generateCommand = command({
 	name: "generate",
@@ -19,50 +16,35 @@ export const generateCommand = command({
 		...outDir,
 	},
 	async handler(argv) {
-		const har = pipe
-			.of(merge(argv.harFirst, ...argv.harRest))
-			.then((har) => filter(har, isGqlRequest))
-			.then((har) => filter(har, isHttpOk))
-			.call()
+		const har = merge(argv.harFirst, ...argv.harRest)
 
 		const operations = getApiOperations(har)
 
 		const fsWrites: Promise<void>[] = []
 
-		for (const [operationName, entries] of operations) {
-			const requestDataSamples = entries.map(
-				(entry) => JSON.parse(entry.request.postData?.text!) as GqlRequest,
-			)
+		for (const [operation, entries] of operations) {
+			const { requestSamples, responseSamples } = getDataSamples(entries)
 
-			const responseDataSamples = entries.map(
-				(entry) =>
-					JSON.parse(
-						entry.response.content.encoding === "base64"
-							? atob(entry.response.content.text!)
-							: entry.response.content.text!,
-					) as GqlResponse,
-			)
+			if (requestSamples.length === 0 || responseSamples.length === 0) continue
 
-			const query = requestDataSamples[0].query
+			const query = requestSamples[0].query
 
-			const variablesSamples = requestDataSamples.map((sample) =>
+			const variableSamples = requestSamples.map((sample) =>
 				JSON.stringify(sample.variables),
 			)
 
-			const dataSamples = responseDataSamples.map((sample) =>
+			const dataSamples = responseSamples.map((sample) =>
 				JSON.stringify(sample.data),
 			)
 
-			const result = await generate(
-				operationName,
+			const result = await generateRequestFile({
+				operation,
 				query,
-				variablesSamples,
 				dataSamples,
-			)
+				variableSamples,
+			})
 
-			const filename = `${operationName
-				.replaceAll(/(?<=[a-z])([A-Z])/g, "_$1")
-				.toLowerCase()}.go`
+			const filename = getFileName(operation)
 
 			const path = resolve(argv.outDir, filename)
 
@@ -75,20 +57,8 @@ export const generateCommand = command({
 	},
 })
 
-function getApiOperations(har: Har) {
-	const operations = new Map<string, [Entry, ...Entry[]]>()
+function getFileName(operation: string) {
+	const base = operation.replaceAll(/(?<=[a-z])([A-Z])/g, "_$1").toLowerCase()
 
-	for (const entry of har.log.entries) {
-		const { operationName } = JSON.parse(
-			entry.request.postData?.text!,
-		) as GqlRequest
-
-		if (operations.has(operationName)) {
-			operations.get(operationName)?.push(entry)
-		} else {
-			operations.set(operationName, [entry])
-		}
-	}
-
-	return operations
+	return `${base}.go`
 }
