454a511e5f07079c97ff4dbff5411c4ec76cd0b5
bgenia <bgenia@student.21-school.ru>
2023-08-27 04:17:16 +0300

    chore: removed old stuff

--- src/har/isGqlApiRequest.ts
+++
@@ -1,6 +0,0 @@
-import { Entry } from "har-format"
-import { S21_GQL_ENDPOINT } from "@/constants"
-
-export function isGqlApiRequest(entry: Entry) {
-	return entry.request.url === S21_GQL_ENDPOINT
-}
