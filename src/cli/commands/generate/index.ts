c6d83eb6f93633aa28b950e35ee0b92afd36e985
bgenia <bgenia@student.21-school.ru>
2023-08-27 04:18:47 +0300

    refactor: fixed generate command

--- src/cli/commands/generate/index.ts
+++ src/cli/commands/generate/index.ts
@@ -1,30 +1,29 @@
 import { writeFile } from "fs/promises"
 import { resolve } from "path"
-import { command, option } from "cmd-ts"
-import { ExistingPath } from "cmd-ts/batteries/fs"
+import { command } from "cmd-ts"
 import { Entry, Har } from "har-format"
-import { harFiles } from "@/cli/arguments/HarFile"
+import { harFiles } from "@/cli/arguments/harFiles"
+import { outDir } from "@/cli/arguments/outDir"
 import { generate } from "@/cli/commands/generate/generate"
 import { GqlRequest } from "@/gql/GqlRequest"
 import { GqlResponse } from "@/gql/GqlResponse"
-import { combineHars } from "@/har/combineHars"
-import { isGqlApiRequest } from "@/har/isGqlApiRequest"
+import { filter, isGqlRequest, isHttpOk } from "@/har/filter"
+import { merge } from "@/har/merge"
+import { pipe } from "@/utils/pipe"
 
 export const generateCommand = command({
 	name: "generate",
 	description: "Generate request source code for s21client",
 	args: {
 		...harFiles,
-		outDir: option({
-			short: "o",
-			long: "out-dir",
-			defaultValue: () => process.cwd(),
-			description: "Output file directory",
-			type: ExistingPath,
-		}),
+		...outDir,
 	},
-	async handler({ harFirst, harRest, outDir }) {
-		const har = combineHars([harFirst, ...harRest])
+	async handler(argv) {
+		const har = pipe
+			.of(merge(argv.harFirst, ...argv.harRest))
+			.then((har) => filter(har, isGqlRequest))
+			.then((har) => filter(har, isHttpOk))
+			.call()
 
 		const operations = getApiOperations(har)
 
@@ -36,7 +35,12 @@ export const generateCommand = command({
 			)
 
 			const responseDataSamples = entries.map(
-				(entry) => JSON.parse(entry.response.content.encoding === "base64" ? atob(entry.response.content.text!) : entry.response.content.text!) as GqlResponse,
+				(entry) =>
+					JSON.parse(
+						entry.response.content.encoding === "base64"
+							? atob(entry.response.content.text!)
+							: entry.response.content.text!,
+					) as GqlResponse,
 			)
 
 			const query = requestDataSamples[0].query
@@ -60,7 +64,7 @@ export const generateCommand = command({
 				.replaceAll(/(?<=[a-z])([A-Z])/g, "_$1")
 				.toLowerCase()}.go`
 
-			const path = resolve(outDir, filename)
+			const path = resolve(argv.outDir, filename)
 
 			const promise = writeFile(path, result)
 
@@ -72,11 +76,9 @@ export const generateCommand = command({
 })
 
 function getApiOperations(har: Har) {
-	const apiEntries = har.log.entries.filter(isGqlApiRequest).filter(e => e.response.status == 200)
-
 	const operations = new Map<string, [Entry, ...Entry[]]>()
 
-	for (const entry of apiEntries) {
+	for (const entry of har.log.entries) {
 		const { operationName } = JSON.parse(
 			entry.request.postData?.text!,
 		) as GqlRequest
