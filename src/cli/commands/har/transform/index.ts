8436b82d3f0cc8085cd75c277035c1ce40e935f6
bgenia <bgenia@student.21-school.ru>
2023-12-08 18:27:09 +0300

    feat: implement takeUnique har merge strategy

--- src/cli/commands/har/transform/index.ts
+++ src/cli/commands/har/transform/index.ts
@@ -9,12 +9,55 @@ import {
 	optional,
 	string,
 } from "cmd-ts"
+import { Har } from "har-format"
+import { produce } from "immer"
 import { harFiles } from "@/cli/arguments/harFiles"
 import { NewFile } from "@/cli/arguments/types/NewFile"
 import { matchGqlOperation } from "@/cli/commands/har/transform/matchGqlOperation"
+import { GqlRequest } from "@/gql/GqlRequest"
 import { clean } from "@/har/clean"
 import { filter, isGqlRequest } from "@/har/filter"
 import { merge } from "@/har/merge"
+
+export function mergeUnique(target: Har, ...sources: Har[]) {
+	if (sources.length === 0) return target
+
+	const uniqueOperations = new Set<string>()
+
+	return produce(target, (draft) => {
+		for (const source of sources) {
+			const sourceUniqueOperations = new Set<string>()
+
+			for (const entry of source.log.entries) {
+				if (!isGqlRequest(entry)) {
+					continue
+				}
+
+				const requestData = entry.request.postData?.text
+
+				if (!requestData) {
+					continue
+				}
+
+				const gqlRequest = JSON.parse(requestData) as GqlRequest
+
+				const { operationName } = gqlRequest
+
+				if (uniqueOperations.has(operationName)) {
+					continue
+				}
+
+				draft.log.entries.push(entry)
+
+				sourceUniqueOperations.add(operationName)
+			}
+
+			for (const operation of sourceUniqueOperations) {
+				uniqueOperations.add(operation)
+			}
+		}
+	})
+}
 
 export const transformCommand = command({
 	name: "transform",
@@ -49,6 +92,13 @@ export const transformCommand = command({
 			description: "Inverse filter by GQL operation name(s)",
 			type: array(string),
 		}),
+		takeUnique: flag({
+			short: "q",
+			long: "take-unique",
+			description:
+				"When merging, takes only unique (new) GQL operations from each har file in order",
+			defaultValue: () => false,
+		}),
 		clean: flag({
 			short: "c",
 			long: "clean",
@@ -64,7 +114,9 @@ export const transformCommand = command({
 		...harFiles,
 	},
 	async handler(argv) {
-		let har = merge(argv.harFirst, ...argv.harRest)
+		const mergeStrategy = argv.takeUnique ? mergeUnique : merge
+
+		let har = mergeStrategy(argv.harFirst, ...argv.harRest)
 
 		if (argv.filterGql || argv.filterOperations.length > 0) {
 			har = filter(har, isGqlRequest)
